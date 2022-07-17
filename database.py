033c2296980e4d6ed9b3e1b37450706c1b6bdbb0
Prodigity <prodigity@ackspace.nl>
2022-07-17 14:06:09 +0200

    In an effort to prevent float errors with cash, I introduced an even bigger error. Depositing 0.1 would result in 1 cents being deposited instead of 10. That is now fixed.

--- database.py
+++ database.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-with open("credentials.txt", 'r') as c_fh:
-	username, password = c_fh.readline().strip().split(":")
 
 #SQLALCHEMY_DATABASE_URI = "sqlite:///ackbar.db"
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{username}:{password}@ackbar-vm/ackbar?charset=utf8mb4"
+with open("../connection.txt", 'r') as c_fh:
+	SQLALCHEMY_DATABASE_URI = c_fh.readline().strip()
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URI,
